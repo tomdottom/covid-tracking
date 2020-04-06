5f89d7ae5d7e1394d29c8574158a335d895070ac
Tom Marks <thomas.o.marks@gmail.com>
2020-04-05 22:16:50 -0400

    feat(): Use git-dolt to checkout data repo

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #/bin/sh
 
+export TMPDIR=./tmp
+
+git dolt fetch covid-tracking.git-dolt || true
+
+cd covid-tracking
+dolt checkout master
+dolt pull
+
 curl http://covidtracking.com/api/states/daily.csv > states-daily.csv
 dolt table import --update-table states_daily states-daily.csv
+dolt add states_daily
+dolt commit -m "Update states_daily $(date --iso=second)"
+dolt push --set-upstream origin master
 
